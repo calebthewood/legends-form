24f0ef753f44806129a0557cd48a3d6b134c2cc8
calebthewood <calebwood.cs@gmail.com>
2023-05-14 13:43:20 -0700

    Adds FileInput for audio-video

---
+++ src/Forms/FileInput.tsx
@@ -0,0 +1,52 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+
+
+
+interface IFileInputProps {
+  type: 'audio' | 'video';
+  updateMain: (p: File | null) => void;
+}
+
+export function FileInput({ type, updateMain }: IFileInputProps) {
+
+  const [file, setFile] = useState<File | null>(null);
+  const [error, setError] = useState(false);
+
+  const fileTypes = {
+    audio: {
+      accepts: 'audio/*',
+      label: 'Upload Audio Clip'
+    },
+    video: {
+      accepts: 'video/*',
+      label: 'Upload Video Clip'
+    }
+  };
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    if (!file) setError(true);
+    if (!event)
+      updateMain(file);
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setError(false);
+    const newFile = event.target.files?.[0];
+    if (!newFile) return;
+    setFile(newFile);
+  }
+
+  return (
+    <form action="" onSubmit={handleSubmit} encType="multipart/form-data">
+      <label htmlFor="file-input">{fileTypes[type].label}</label>
+      <input
+        type="file"
+        name="file-input"
+        className={error ? 'input-error' : ''}
+        accept={fileTypes[type].accepts}
+        onChange={handleChange} />
+      <button>Next</button>
+    </form>
+  );
+}
