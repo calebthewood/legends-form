c27b29bb840c07133d2a888bf3ee28e39e99052c
calebthewood <calebwood.cs@gmail.com>
2023-05-17 15:41:27 -0700

    Makes list fetch updated data

--- src/Common/StateList.tsx
+++ src/Common/StateList.tsx
@@ -1,31 +1,24 @@
+import { useSupabase } from "./useSupabase";
 
-interface IStateListFields {
-  textFields: string[] | null;
-  fileFields: string[] | null;
-  multiChoiceFields: string[] | null;
-}
 
-export function StateList({ textFields, fileFields, multiChoiceFields }: IStateListFields) {
+export function StateList() {
+
+  const activity = useSupabase('activities', '*');
+
+  console.log(activity);
+
+  const results = ['question_1', 'question_2', 'question_3', 'question_4', 'question_5'];
 
   return (
     <div className='form-item state-list'>
-      <ol>
-        <h3>Text List:</h3>
-        {textFields && textFields.map((field, i) =>
-          <li key={`answers-${i * 7}`}>{field}</li>)}
-      </ol>
-
-      <ol>
-        <h3>File List:</h3>
-        {fileFields && fileFields.map((field, i) =>
-          <li key={`answers-${i * 7}`}>{field}</li>)}
-      </ol>
-
-      <ol>
-        <h3>MultiChoice List:</h3>
-        {multiChoiceFields && multiChoiceFields.map((field, i) =>
-          <li key={`answers-${i * 7}`}>{field}</li>)}
-      </ol>
+      <div className="form-body">
+        <h2>Results, fetched from Supabase</h2>
+        <ol>
+          {activity ?
+            results.map((question, i) => <li key={`answers-${i * 7}`}>{activity[question]?.response || activity[question]?.file_name }</li>) :
+            <h3>Loading...</h3>}
+        </ol>
+      </div>
     </div>
   );
 }
