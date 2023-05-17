b3e0406d587f6e789eb88f66660fe3ad0adf0a65
calebthewood <calebwood.cs@gmail.com>
2023-05-17 15:35:35 -0700

    Removes state lists, adds supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useSupabase } from './db';
+import { useState } from 'react';
+import { useSupabase } from './Common/useSupabase'
 import { TextInput } from './Forms/TextInput';
 import { TextAreaInput } from './Forms/TextAreaInput';
 import { FileInput } from './Forms/FileInput';
@@ -16,59 +16,29 @@ const ANIMATION_DURATION_MS = 200; // based on 'grow' and 'shrink' animations in
 function App() {
 
   /* Form Field State */
-  const [textFields, setTextFields] = useState<string[] | null>(null);
-  const [fileFields, setFileFields] = useState<string[] | null>(null);
-  const [multiChoiceFields, setMultiChoiceFields] = useState<string[] | null>(null);
-  const activity = useSupabase({ tableName: 'activities', select: '*' });
+  const activity = useSupabase('activities', '*');
 
   /* Carousel Related State */
   const [transition, setTransition] = useState('animated-entry');
-  const [isSubmitted, setIsSubmitted] = useState(true);
   const [step, setStep] = useState(0);
-
-
 
   function renderFormField(step: number) {
     switch (step) {
       case 1:
-        return <TextInput field={activity.question_1} updateMain={updateTextFields} />;
+        return <TextInput prevStep={prevStep} nextStep={nextStep} step={step} field={activity.question_1} />;
       case 2:
-        return <TextAreaInput field={activity.question_2} updateMain={updateTextFields} />;
+        return <TextAreaInput prevStep={prevStep} nextStep={nextStep} step={step} field={activity.question_2} />;
       case 3:
-        return <MultipleChoiceInput field={activity.question_3} updateMain={updateMultiChoiceFields} />;
+        return <MultipleChoiceInput prevStep={prevStep} nextStep={nextStep} step={step} field={activity.question_3} />;
       case 4:
-        return <FileInput field={activity.question_4} updateMain={updateFileFields} />;
+        return <FileInput prevStep={prevStep} nextStep={nextStep} step={step} field={activity.question_4} />;
       case 5:
-        return <FileInput field={activity.question_5} updateMain={updateFileFields} />;
+        return <FileInput prevStep={prevStep} nextStep={nextStep} step={step} field={activity.question_5} />;
       case 6:
-        return <StateList textFields={textFields} fileFields={fileFields} multiChoiceFields={multiChoiceFields} />;
+        return <StateList />;
       default:
         return <Welcome />;
     }
-  }
-
-  /** Passed to text-based input components to update textFields and isSubmitted */
-  function updateTextFields(newValue: string | null) {
-    if (!newValue) return;
-    const newData = textFields ? [...textFields, newValue] : [newValue];
-    setTextFields(newData);
-    setIsSubmitted(true);
-  }
-
-  /** Passed to file-based input components to update fileFields and isSubmitted */
-  function updateFileFields(newValue: string | null) {
-    if (!newValue) return;
-    const newData = fileFields ? [...fileFields, newValue] : [newValue];
-    setFileFields(newData);
-    setIsSubmitted(true);
-  }
-
-  /** Passed to radio field components to update multiChoiceFields and isSubmitted */
-  function updateMultiChoiceFields(newValue: string | null) {
-    if (!newValue) return;
-    const newData = multiChoiceFields ? [...multiChoiceFields, newValue] : [newValue];
-    setMultiChoiceFields(newData);
-    setIsSubmitted(true);
   }
 
   /** NextStep
@@ -80,7 +50,6 @@ function App() {
       setStep(step < STEP_MAX ? step + 1 : step);
       setTransition('animated-entry');
     }, ANIMATION_DURATION_MS);
-    setIsSubmitted(false);
   }
 
   /** PrevStep
@@ -92,22 +61,19 @@ function App() {
       setStep(step > STEP_MIN ? step - 1 : step);
       setTransition('animated-entry');
     }, ANIMATION_DURATION_MS);
-    setIsSubmitted(true);
   }
 
   return (
     <>
-      <h1>Legends</h1>
+      <h1>Form Carousel</h1>
       <div className='form-container'>
-        <div className='content'>
-          <div className={transition}>
+        <div className={`form-content ${transition}`}>
             {activity ? renderFormField(step) : <h1>Loading...</h1>}
-          </div>
         </div>
-        <div className={step === STEP_MIN ? 'nav-start nav-btns' : 'nav-btns'}>
-          {step > STEP_MIN && <button onClick={prevStep} disabled={step <= 0}>Back</button>}
-          {step < STEP_MAX && <button onClick={nextStep} disabled={!isSubmitted} >Next</button>}
-        </div>
+        {step === 0 &&
+        <div className='nav-start nav-btns'>
+           <button className='next' onClick={nextStep}>Start!</button>
+        </div>}
       </div>
     </>
   );
